9a03cef65e2963e8e91767c4cd69f19468f937e3
Mohsen <mohsen0@users.noreply.github.com>
2024-08-30 18:25:43 +0100

    Fix shasum

--- Formula/awswhoiam.rb
+++ Formula/awswhoiam.rb
@@ -7,13 +7,13 @@ class Awswhoiam < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mohsen0/awswhoiam/releases/download/v#{version}/awswhoiam_darwin_arm64"
-      sha256 "9df9a11ce3ef09e54588c48361758c7fedb82e1920af007ba7256133dccea783"
+      sha256 "97efa13e2f919b6ebf4fca8faf364462fa1e9b58a023e2f46a80f51bf85a4fbd"
       def install
         bin.install "awswhoiam_darwin_arm64" => "awswhoiam"
       end
     else
       url "https://github.com/mohsen0/awswhoiam/releases/download/v#{version}/awswhoiam_darwin_amd64"
-      sha256 "97efa13e2f919b6ebf4fca8faf364462fa1e9b58a023e2f46a80f51bf85a4fbd"
+      sha256 "9df9a11ce3ef09e54588c48361758c7fedb82e1920af007ba7256133dccea783"
       def install
         bin.install "awswhoiam_darwin_amd64" => "awswhoiam"
       end
